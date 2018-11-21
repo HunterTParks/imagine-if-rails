5356d3b9341317f857d4839d5c9cef22e43461a3
Hunter Parks <hunter.thomas.parks@gmail.com>
2018-11-21 14:03:01 -0800

    add 'enabled' to User schema
    This will be used for users that want to disable their account OR they
    are forcibly removed via moderator

--- db/schema.rb
+++ db/schema.rb
@@ -10,23 +10,9 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2018_11_13_052250) do
+ActiveRecord::Schema.define(version: 2018_11_21_220213) do
 
-  create_table "users", force: :cascade do |t|
-    t.string "email", default: "", null: false
-    t.string "encrypted_password", default: "", null: false
-    t.string "reset_password_token"
-    t.datetime "reset_password_sent_at"
-    t.datetime "remember_created_at"
-    t.integer "sign_in_count", default: 0, null: false
-    t.datetime "current_sign_in_at"
-    t.datetime "last_sign_in_at"
-    t.string "current_sign_in_ip"
-    t.string "last_sign_in_ip"
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
-    t.index ["email"], name: "index_users_on_email", unique: true
-    t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
-  end
+# Could not dump table "users" because of following StandardError
+#   Unknown type 'bool' for column 'enabled'
 
 end
--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -1,5 +1,8 @@
 require "rails_helper"
 
 RSpec.describe User, :type => :model do
-  it "should successfully create a user"
+  context "When creating a new user it..." do
+    it "should create a user successfully"
+    it "should return an error if not created successfully"
+  end
 end
