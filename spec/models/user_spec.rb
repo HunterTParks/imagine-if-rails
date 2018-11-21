71ed056d5c848f55548782b84ad426b334a4181f
Hunter Parks <hunter.thomas.parks@gmail.com>
2018-11-21 14:59:39 -0800

    refactor test

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -3,9 +3,7 @@ require "rails_helper"
 RSpec.describe User, :type => :model do
   context "When creating a new user it..." do
     it "should create a user successfully" do
-      user = create(:user)
-      user2 = User.first
-      expect(user).to eq(user2)
+      expect(create(:user)).to be_valid
     end
     it "should return an error if not created successfully"
   end
