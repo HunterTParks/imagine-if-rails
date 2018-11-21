862d70e2a4181c52c02c7e40526cf82db15cd1e5
Hunter Parks <hunter.thomas.parks@gmail.com>
2018-11-21 14:18:18 -0800

    add username field to users

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2018_11_21_220213) do
+ActiveRecord::Schema.define(version: 2018_11_21_221640) do
 
 # Could not dump table "users" because of following StandardError
 #   Unknown type 'bool' for column 'enabled'
