f00ab8f2b6b3e07a60f7ed160e23bee23fec5720
Hunter Parks <hunter.thomas.parks@gmail.com>
2018-11-21 14:50:35 -0800

    add tests for successfully creating users

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -2,7 +2,11 @@ require "rails_helper"
 
 RSpec.describe User, :type => :model do
   context "When creating a new user it..." do
-    it "should create a user successfully"
+    it "should create a user successfully" do
+      user = create(:user)
+      user2 = User.first
+      expect(user).to eq(user2)
+    end
     it "should return an error if not created successfully"
   end
 end
