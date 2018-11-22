13ba9955ad6568a5ad2dfb810deefebaf57a1301
Hunter Parks <hunter.thomas.parks@gmail.com>
2018-11-21 17:09:31 -0800

    add associations to users and posts

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,5 +1,6 @@
 class User < ApplicationRecord
   validates :email, :username, :password, presence: true
+  has_many :posts
 
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :trackable, :validatable
