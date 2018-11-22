f00a214e971df7ca3e3c9f9b9e46d676f178c613
Hunter Parks <hunter.thomas.parks@gmail.com>
2018-11-21 22:19:23 -0800

    remove low-priority that wasn't worth having until later

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -13,6 +13,5 @@ RSpec.describe User, :type => :model do
     it "should create a user successfully" do
       expect(create(:user)).to be_valid
     end
-    it "should return an error if not created successfully"
   end
 end
