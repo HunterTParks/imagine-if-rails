523b7541d31ab783ed12352122d59e3abbbc9d4e
Hunter Parks <hunter.thomas.parks@gmail.com>
2018-11-22 13:38:31 -0800

    add validation specs for messages

--- spec/models/message_spec.rb
+++ spec/models/message_spec.rb
@@ -6,4 +6,10 @@ RSpec.describe :Message, :type => :model do
       expect(Message.reflect_on_association(:user).macro).to eq(:belongs_to)
     end
   end
+  context "When creating a message, it" do
+    it "should create a message successfully"
+    it "should not be valid without a first user"
+    it "should not be valid without a second user"
+    it "should not be valid without a body"
+  end
 end
