425d28e504ba9e56bed585279b894049f5d25410
Hunter Parks <hunter.thomas.parks@gmail.com>
2018-11-21 21:18:08 -0800

    edit associations between users, posts, and comments
    The associations are as follows: Users can make many posts and comments,
    Post belong to one user and have many comments, comments only belong to
    one post and belong to one user.

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,7 @@
 class User < ApplicationRecord
   validates :email, :username, :password, presence: true
   has_many :posts
+  has_many :comment
 
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :trackable, :validatable
