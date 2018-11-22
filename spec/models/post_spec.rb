a97258919f91ee79a18827de0e201eb9c36a95d9
Hunter Parks <hunter.thomas.parks@gmail.com>
2018-11-22 11:35:27 -0800

    add validation specs for posts

--- spec/models/post_spec.rb
+++ spec/models/post_spec.rb
@@ -9,4 +9,11 @@ RSpec.describe :Post, :type => :model do
       expect(Post.reflect_on_association(:comment).macro).to eq(:has_many)
     end
   end
+
+  context "When a post is created, it" do
+    it "should create a post successfully"
+    it "should not be valid without a user"
+    it "should not be valid without a title"
+    it "should not be valid without a body"
+  end
 end
