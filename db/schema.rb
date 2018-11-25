6f1808d053de772a76f3e61bf604daab33ea1b3f
Hunter Parks <hunter.thomas.parks@gmail.com>
2018-11-25 00:12:31 -0800

    add references to user and posts model

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2018_11_25_072807) do
+ActiveRecord::Schema.define(version: 2018_11_25_080628) do
 
   create_table "admins", force: :cascade do |t|
     t.datetime "created_at", null: false
@@ -21,6 +21,10 @@ ActiveRecord::Schema.define(version: 2018_11_25_072807) do
     t.string "message"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.integer "users_id"
+    t.integer "posts_id"
+    t.index ["posts_id"], name: "index_comments_on_posts_id"
+    t.index ["users_id"], name: "index_comments_on_users_id"
   end
 
   create_table "messages", force: :cascade do |t|
