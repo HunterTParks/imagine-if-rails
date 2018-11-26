2a2afb153e853507359b5336735b41b9ecb5ce56
Hunter Parks <hunter.thomas.parks@gmail.com>
2018-11-25 18:34:49 -0800

    add api namespace and a mailboxer controller
    The namespace is for handling all user interactions through an API (duh)
    and I added the mailboxer controller that will handle sending and
    displaying messages

---
+++ app/controllers/api/mailboxer_controller.rb
@@ -0,0 +1,10 @@
+class Api::MailboxerController < ApplicationController
+  def send_message
+  end
+
+  def inbox
+  end
+
+  def trash
+  end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,8 @@
 Rails.application.routes.draw do
+  namespace :api do
+    get 'mailboxer/send_message'
+    get 'mailboxer/inbox'
+    get 'mailboxer/trash'
+  end
   devise_for :users
 end
