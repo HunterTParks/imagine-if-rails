f4adce271ffeaf7e5a9dfdd28fe3852f08c1bbd7
Hunter Parks <hunter.thomas.parks@gmail.com>
2018-11-25 17:10:24 -0800

    add specs for message http requests

--- spec/requests/mailboxer.rb
+++ spec/requests/mailboxer.rb
@@ -1,5 +1,10 @@
 require 'rails_helper'
 
 RSpec.describe "Mailboxer", :type => :request do
-  
+  context "When a user sends a message, it" do
+    it "should successfully send a message"
+    it "should not be valid without a body"
+    it "should not be valid without a user"
+    it "should not be valid without a recipient"
+  end
 end
